31f0cb4143aad62dc224cda35b6968ecafa85083
Ubuntu <ubuntu@ip-172-31-29-217.us-east-2.compute.internal>
2021-10-18 22:00:58 +0000

    update project name

--- coffee-shop-dbt/models/customers.sql
+++ coffee-shop-dbt/models/customers.sql
@@ -7,7 +7,7 @@ select c.id as customer_id
 , min(created_at) as first_order_at
 , count(distinct o.id) as num_orders
 from {{ source('coffee_shop', 'customers') }} c
-join  {{source('coffee_shop', 'orders') }} o
+join  {{ source('coffee_shop', 'orders') }} o
 on c.id = o.customer_id
 group by 1,2,3
 order by 4 
