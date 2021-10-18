e324b700a1921b06623417a66f9eb67314ac935e
Ubuntu <ubuntu@ip-172-31-29-217.us-east-2.compute.internal>
2021-10-18 21:44:08 +0000

    add source file and to code

--- coffee-shop-dbt/models/customers.sql
+++ coffee-shop-dbt/models/customers.sql
@@ -6,8 +6,8 @@ select c.id as customer_id
 , c.email 
 , min(created_at) as first_order_at
 , count(distinct o.id) as num_orders
-from analytics-engineers-club.coffee_shop.customers c
-join  analytics-engineers-club.coffee_shop.orders o
+from {{ source('coffee_shop', 'customers') }} c
+join  {{source('coffee_shop', 'orders') }} o
 on c.id = o.customer_id
 group by 1,2,3
 order by 4 
